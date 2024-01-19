d01ad78c96ca23996d72ffb29f29286d46cceeda
Valentin Virzi <v.virzi@hotmail.com>
2024-01-19 17:27:36 -0300

    almost finished

--- main.js
+++ main.js
@@ -10,14 +10,131 @@
 // Habrá también una tabla que muestre las últimas 5 elecciones hechas por el contrincante
 // Además existirá un botón que al oprimirlo reiniciará la partida actual, es decir: Puntajes, Elecciones actuales (de cada participante), Texto con resultado de la Ronda & Tabla de Últimas elecciones tomadas por el contrincante
 
-function makeChoicePC() {
-  let choice = Math.random();
-  if (choice < 0.33) {
-    choice = "Rock";
-  } else if (choice > 0.66) {
-    choice = "Paper";
-  } else {
-    choice = "Scissors";
+const containerLatestChoices = document.getElementById(
+  "containerLatestChoices"
+);
+const currentPickPC = document.getElementById("currentPickPC");
+const currentPickUser = document.getElementById("currentPickUser");
+const scoreUser = document.getElementById("scoreUser");
+const scorePC = document.getElementById("scorePC");
+const roundOutcome = document.getElementById("roundOutcome");
+const btnRock = document.getElementById("btnRock");
+const btnPaper = document.getElementById("btnPaper");
+const btnScissors = document.getElementById("btnScissors");
+const btnRestart = document.getElementById("btnRestart");
+let userScore = 0;
+let PCScore = 0;
+
+function displayPick(choice, element) {
+  switch (choice) {
+    case "Rock":
+      element.setAttribute("src", "img/rock.png");
+      break;
+    case "Paper":
+      element.setAttribute("src", "img/paper.png");
+      break;
+    case "Scissors":
+      element.setAttribute("src", "img/scissors.png");
+      break;
+    default:
+      element.setAttribute(
+        "src",
+        "https://www.svgrepo.com/show/458579/cancel.svg"
+      );
   }
-  return choice;
 }
+
+function makeChoiceUser(e) {
+  let choiceUser;
+  switch (e.target.id) {
+    case "Rock":
+      choiceUser = "Rock";
+      break;
+    case "Paper":
+      choiceUser = "Paper";
+      break;
+    case "Scissors":
+      choiceUser = "Scissors";
+      break;
+    default:
+      choiceUser = "";
+  }
+  return choiceUser;
+}
+
+function makeChoicePC() {
+  let choicePC = Math.random();
+  if (choicePC < 0.33) {
+    choicePC = "Rock";
+  } else if (choicePC > 0.66) {
+    choicePC = "Paper";
+  } else {
+    choicePC = "Scissors";
+  }
+  return choicePC;
+}
+
+function getOutcome(choiceUser, choicePC) {
+  if (choiceUser == choicePC) {
+    roundOutcome.textContent = "It's a draw :|";
+    roundOutcome.classList.remove("won", "lost");
+    roundOutcome.classList.add("draw");
+  } else if (
+    (choiceUser == "Rock" && choicePC == "Paper") ||
+    (choiceUser == "Paper" && choicePC == "Scissors") ||
+    (choiceUser == "Scissors" && choicePC == "Rock")
+  ) {
+    roundOutcome.textContent = "You lose :(";
+    roundOutcome.classList.remove("won", "draw");
+    roundOutcome.classList.add("lost");
+    ++PCScore;
+    scorePC.textContent = PCScore;
+  } else if (
+    (choicePC == "Rock" && choiceUser == "Paper") ||
+    (choicePC == "Paper" && choiceUser == "Scissors") ||
+    (choicePC == "Scissors" && choiceUser == "Rock")
+  ) {
+    roundOutcome.textContent = "You win :)";
+    roundOutcome.classList.remove("draw", "lost");
+    roundOutcome.classList.add("won");
+    ++userScore;
+    scoreUser.textContent = userScore;
+  }
+}
+
+function restartGame() {
+  displayPick(undefined, currentPickPC);
+  displayPick(undefined, currentPickUser);
+  userScore = 0;
+  PCScore = 0;
+  scoreUser.textContent = userScore;
+  scorePC.textContent = PCScore;
+}
+
+btnRock.addEventListener("pointerdown", (e) => {
+  let userChoice = makeChoiceUser(e);
+  let PCChoice = makeChoicePC();
+  displayPick(PCChoice, currentPickPC);
+  displayPick(userChoice, currentPickUser);
+  getOutcome(userChoice, PCChoice);
+});
+btnPaper.addEventListener("pointerdown", (e) => {
+  let userChoice = makeChoiceUser(e);
+  let PCChoice = makeChoicePC();
+  displayPick(PCChoice, currentPickPC);
+  displayPick(userChoice, currentPickUser);
+  getOutcome(userChoice, PCChoice);
+});
+btnScissors.addEventListener("pointerdown", (e) => {
+  let userChoice = makeChoiceUser(e);
+  let PCChoice = makeChoicePC();
+  displayPick(PCChoice, currentPickPC);
+  displayPick(userChoice, currentPickUser);
+  getOutcome(userChoice, PCChoice);
+});
+btnRestart.addEventListener("pointerdown", (e) => {
+  restartGame();
+});
+document.addEventListener("DOMContentLoaded", (e) => {
+  restartGame();
+});
